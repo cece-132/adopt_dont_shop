4ad3baa355a5752ef7502b31bc16d6f006761d6b
Candace Eckels <candace.eckels@gmail.com>
2022-09-07 17:31:58 -0600

    accepting and rejecting doesn't affect other applications

--- spec/features/admin/applicants/show_spec.rb
+++ spec/features/admin/applicants/show_spec.rb
@@ -54,5 +54,46 @@ RSpec.describe Applicant do
       end
     end
 
+
+    it 'for every pet i see a button to reject that application for that pet' do
+      @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      
+      @app_1 = Applicant.create!(name: "Billy Wahl", street_address: "123 S Street Way",
+        city: "Denver", state: "CO", zip_code: "80123", description: "I Like Dogs", status: 'Pending')
+
+      @app_2 = Applicant.create!(name: "Mageneto", street_address: "Evil Mansion Way",
+        city: "Denver", state: "CO", zip_code: "80123", description: "I need a heavy metal dog", status: 'Pending')
+    
+  
+      @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+      @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+
+      pet_app1 = PetApplicant.create!(pet_id: @pet_1.id, applicant_id: @app_1.id)
+      pet_app2 = PetApplicant.create!(pet_id: @pet_2.id, applicant_id: @app_1.id)
+
+      pet_app3 = PetApplicant.create!(pet_id: @pet_1.id, applicant_id: @app_2.id)
+      pet_app4 = PetApplicant.create!(pet_id: @pet_2.id, applicant_id: @app_2.id)
+
+      visit "/admin/applicants/#{@app_1.id}"
+
+      within ".pets" do
+        within "#pet-#{@pet_1.id}" do
+          expect(page).to have_button("Reject Pet Application")
+          click_button "Reject Pet Application"
+          expect(page).to have_content("Rejected")
+        end
+      end
+
+      visit "/admin/applicants/#{@app_2.id}"
+
+      within ".pets" do
+        within "#pet-#{@pet_1.id}" do
+          expect(page).to have_button("Accept Pet Application")
+          expect(page).to have_button("Reject Pet Application")
+        end
+      end
+
+    end
+
   end
 end
